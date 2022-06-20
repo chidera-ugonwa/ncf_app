fc871638f169e368f0505e87c4046aa9a6966218
FlutLab.io <>
2022-06-20 01:55:52 +0000

    Autosave

--- lib/screens/library/library.dart
+++ lib/screens/library/library.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:vimeo_video_player/vimeo_video_player.dart';
 
-class Library extends StatelessWidget {
+class Library extends StatefulWidget {
   const Library({Key? key}) : super(key: key);
+
+  @override
+  State<Library> createState() => _LibraryState();
+}
+
+class _LibraryState extends State<Library> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('1'),
-    );
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: VimeoVideoPlayer(
+            url: '',
+          ),
+        ));
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import '../models/user.dart';
 
-//import '../screens/authenticate/verify_email.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
